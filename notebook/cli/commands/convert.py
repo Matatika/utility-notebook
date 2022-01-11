a710f4573bebcffc1a79b80d4ca388d2f175c84e
danielpdwalker <danielpdwalker@gmail.com>
2022-01-11 12:37:08 +0000

    Updated run and convert commands to accept an env var as their default value

--- notebook/cli/commands/convert.py
+++ notebook/cli/commands/convert.py
@@ -4,6 +4,7 @@
 
 import sys
 import click
+import os
 
 from notebook.utils import get_list_of_paths
 from notebook.notebook_converter import convert_notebook
@@ -13,13 +14,16 @@ from .root import notebook
 @notebook.command(
     "convert", short_help="Convert a Jupyter notebook, or all notebooks in a directory"
 )
-@click.argument("path", type=click.Path(exists=True))
+@click.argument(
+    "path", type=click.Path(exists=True), default=os.getenv("NOTEBOOK_PATH")
+)
 @click.option(
     "--format",
     "-f",
     "format_",
     type=click.STRING,
     help="What format you want to convert your notebook into. Supported formats: PDF",
+    default=os.getenv("NOTEBOOK_FORMAT"),
 )
 def convert(path, format_):
     """Convert a notebook or all notebooks in a directory"""
--- notebook/cli/commands/run.py
+++ notebook/cli/commands/run.py
@@ -1,6 +1,7 @@
 """CLI 'run' command"""
 
 import click
+import os
 
 from notebook.notebook_converter import convert_notebook
 from notebook.notebook_runner import run_notebook
@@ -9,13 +10,16 @@ from .root import notebook
 
 
 @notebook.command("run", short_help="Run the notebook")
-@click.argument("path", type=click.Path(exists=True))
+@click.argument(
+    "path", type=click.Path(exists=True), default=os.getenv("NOTEBOOK_PATH")
+)
 @click.option(
     "--format",
     "-f",
     "format_",
     type=click.STRING,
     help="What format you want to convert your notebook into. Supported formats: PDF",
+    default=os.getenv("NOTEBOOK_FORMAT"),
 )
 def run(path, format_):
     """Run a notebook or all notebooks in a directory"""
